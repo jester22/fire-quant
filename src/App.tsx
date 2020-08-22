6f413fd5eda24e0cd27b131875e8f26cf7e69039
Jester Temporada <jester.temporada@gmail.com>
2020-08-22 16:17:29 +0800

    Add base code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const electron = window.require('electron');
+const { ipcRenderer } = electron;
+
 function App() {
+
+  useEffect(() => {
+    // setting up an event listener to read data that background process
+		// will send via the main process after processing the data we
+		// send from visiable renderer process
+		ipcRenderer.on('MESSAGE_FROM_BACKGROUND_VIA_MAIN', (event, args) => {
+			console.log(args);
+		});
+
+		// trigger event to start background process
+		// can be triggered pretty much from anywhere after
+		// you have set up a listener to get the information
+		// back from background process, as I have done in line 13
+		ipcRenderer.send('START_BACKGROUND_VIA_MAIN', {
+			number: 25,
+		});
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
